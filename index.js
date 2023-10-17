e368e27226d6b792f4a0cacf5161a7b4ab0f9817
nitinjha04 <93567737+nitinjha04@users.noreply.github.com>
2023-10-17 13:26:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ const port = 3000;
 
 const urlModel = require("./models/url");
 
-mongoose
-  .connect(process.env.MONGODB)
-  .then(() => console.log("mongo connected"))
-  .catch((err) => console.error(err));
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 const client = new Client({
   intents: [
