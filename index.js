474bfecdc9478c4224260da617260a0f550cf85c
nitinjha04 <93567737+nitinjha04@users.noreply.github.com>
2023-10-17 13:28:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const urlModel = require("./models/url");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const databaseClient = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -24,13 +24,13 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await databaseClient.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await databaseClient.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await databaseClient.close();
   }
 }
 run().catch(console.dir);
