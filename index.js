4542e81f6e0c8942cea6a85f59697aeb6897df2f
nitinjha04 <nitinjha890@gmail.com>
2023-10-18 14:38:25 +0530

    commit

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ client.on("messageCreate", async (message) => {
 
     // Proceed to create a new short URL
   }
-  if(!message.startsWith("H")|| !message.startsWith("c")){
+  else{
     return message.reply({
       content: "I don't know how to respond to that. For now i can only respond to Hi or create {your desired URL you want to create} ",
     })
